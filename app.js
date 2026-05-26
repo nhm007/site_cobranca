7c2d4209f8ec4ffb5e01807de6e1cf3501e7d50e
nhm007 <82231807+nhm007@users.noreply.github.com>
2026-05-26 15:18:45 -0300

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-
-import { Router } from 'express';
-import * as controller from '../controllers/clients.controller.js';
-
-export const clientsRoutes = Router();
-
-clientsRoutes.get('/', controller.list);
-clientsRoutes.post('/', controller.create);
